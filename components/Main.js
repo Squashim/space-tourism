162f06ff60d655ca15cd4c2cffa296bb7253c963
Squashim <konrad18v1@wp.pl>
2022-09-15 13:31:28 +0200

    Tweaks and fixes

--- components/Main.js
+++ components/Main.js
@@ -3,15 +3,15 @@ import ExploreBtn from "./ExploreBtn";
 
 const Main = () => {
 	return (
-		<div className=' relative p-6 flex flex-col lg:flex-row lg:px-[10rem] items-center justify-between '>
-			<div className='flex flex-col items-center text-center lg:text-left  xs:min-w-[21rem] mx-auto lg:m-0'>
-				<p className='text-blue uppercase font-barlowCondensed text-[1rem] tracking-[2px] xs:text-[1.25rem] xs:tracking-[3.38px] w-full lg:pl-2'>
+		<div className='relative p-6 lg:px-[10rem] items-center flex h-[calc(100vh-144px)] justify-between '>
+			<div className='max-w-[500px] w-full'>
+				<p className='text-blue uppercase font-barlowCondensed text-[1rem] tracking-[2px] xs:text-[1.25rem] lg:pl-2 xs:tracking-[3.38px]  '>
 					So, you want to travel to
 				</p>
-				<h1 className='xs:text-[9.375rem] text-white uppercase text-[5rem] font-bellefair xs:leading-[9.375rem] leading-[6.25rem] mt-4'>
+				<h1 className=' xs:text-[9.375rem] text-white uppercase text-[5rem] font-bellefair xs:leading-[9.375rem] leading-[6.25rem] '>
 					Space
 				</h1>
-				<p className='font-barlow text-blue text-[0.938rem] xs:text-base mt-4 leading-[1.563rem] lg:max-w-[28rem]'>
+				<p className='font-barlow text-blue text-[0.938rem] xs:text-base mt-4 lg:pl-2 leading-[1.563rem] lg:max-w-[28rem]'>
 					Let’s face it; if you want to go to space, you might as well genuinely
 					go to outer space and not hover kind of on the edge of it. Well sit
 					back, and relax because we’ll give you a truly out of this world
@@ -19,7 +19,7 @@ const Main = () => {
 				</p>
 			</div>
 
-			<div className='flex items-center w-full lg:w-[50%] lg:justify-end  justify-center  mt-16 '>
+			<div className='flex w-full lg:justify-end  mt-16 '>
 				<ExploreBtn />
 			</div>
 		</div>
