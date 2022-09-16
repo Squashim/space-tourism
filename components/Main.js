d5eedf0785c44ec756dfbbfa8bfb7d93e7217302
Squashim <konrad18v1@wp.pl>
2022-09-16 17:17:06 +0200

    Added navigation

--- components/Main.js
+++ components/Main.js
@@ -5,6 +5,7 @@ const Main = () => {
 	return (
 		<main className='relative p-6 lg:px-[10rem] flex lg:flex-row flex-col  text-center lg:text-left xs:pt-[6.625rem] pt-[3.625rem] xl:pt-[8rem] '>
 			<div className='flex flex-col w-full items-center justify-center lg:flex-row lg:justify-between lg:items-end'>
+				{/* Text */}
 				<div className='max-w-[460px] w-full flex flex-col xs:gap-6 gap-4'>
 					<p className='text-blue uppercase font-barlowCondensed text-[1rem] tracking-[2.7px] xs:text-[1.25rem]  xs:tracking-[3.38px] lg:text-[1.4rem]'>
 						So, you want to travel to
@@ -13,13 +14,14 @@ const Main = () => {
 						Space
 					</h1>
 					<p className='font-barlow text-blue text-[0.93rem] xs:text-base xs:leading-[1.75rem] leading-[1.56rem] lg:leading-8 lg:text-[1.125rem]'>
-						Let’s face it; if you want to go to space, you might as well
+						Let's face it; if you want to go to space, you might as well
 						genuinely go to outer space and not hover kind of on the edge of it.
-						Well sit back, and relax because we’ll give you a truly out of this
+						Well sit back, and relax because we'll give you a truly out of this
 						world experience!
 					</p>
 				</div>
 
+				{/* Btn */}
 				<ExploreBtn />
 			</div>
 		</main>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/shared/logo.svg";
 import menu from "../assets/shared/icon-hamburger.svg";
 import close from "../assets/shared/icon-close.svg";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
 	const [nav, setNav] = useState(false);
-	const [active, setActive] = useState("home");
+	const [active, setActive] = useState("/");
+
+	const router = useRouter();
+
+	useEffect(() => {
+		switch (router.asPath) {
+			case "/":
+				document.body.className = "home";
+				setActive("/");
+				break;
+			case "/destination":
+				document.body.className = "destination";
+				setActive("/destination");
+
+				break;
+			case "/crew":
+				document.body.className = "crew";
+				setActive("/crew");
+
+				break;
+			case "/technology":
+				document.body.className = "technology";
+				setActive("/technology");
+		}
+	}, [router.asPath]);
 
 	return (
 		<nav className='relative flex items-center justify-between h-[6rem] xs:py-6 xs:pr-0 xs:pl-[3.4rem] p-6 xl:mt-[2.5rem] '>
+			{/* Logo */}
 			<Image src={logo} />
 
 			{/* Decal */}
@@ -21,9 +47,8 @@ const Navbar = () => {
 				<ul className='font-barlowCondensed text-blue text-sm xl:text-base flex uppercase  w-full items-center gap-4 lg:gap-8 xl:gap-12 justify-center'>
 					<Link href='/'>
 						<li
-							onClick={() => setActive("home")}
 							className={
-								active === "home"
+								active === "/"
 									? "xl:flex xl:gap-3 xl:tracking-[2.7px] text-white tracking-[2px] cursor-pointer ease duration-100 border-b-white border-b-4 h-full py-[34px]"
 									: "xl:flex xl:gap-3 xl:tracking-[2.7px] tracking-[2px] text-white cursor-pointer ease duration-100 hover:border-b-white/50 border-b-transparent h-full py-[34px] border-b-4"
 							}>
@@ -32,9 +57,8 @@ const Navbar = () => {
 					</Link>
 					<Link href='/destination'>
 						<li
-							onClick={() => setActive("destination")}
 							className={
-								active === "destination"
+								active === "/destination"
 									? "xl:flex xl:gap-3 xl:tracking-[2.7px] text-white tracking-[2px] cursor-pointer ease duration-100 border-b-white border-b-4 h-full py-[34px]"
 									: "xl:flex xl:gap-3 xl:tracking-[2.7px] tracking-[2px] text-white cursor-pointer ease duration-100 hover:border-b-white/50 border-b-transparent h-full py-[34px] border-b-4"
 							}>
@@ -44,9 +68,8 @@ const Navbar = () => {
 					</Link>
 					<Link href='/crew'>
 						<li
-							onClick={() => setActive("crew")}
 							className={
-								active === "crew"
+								active === "/crew"
 									? "xl:flex xl:gap-3 xl:tracking-[2.7px] text-white tracking-[2px] cursor-pointer ease duration-100 border-b-white border-b-4 h-full py-[34px]"
 									: "xl:flex xl:gap-3 xl:tracking-[2.7px] tracking-[2px] text-white cursor-pointer ease duration-100 hover:border-b-white/50  border-b-transparent h-full py-[34px] border-b-4"
 							}>
@@ -55,9 +78,8 @@ const Navbar = () => {
 					</Link>
 					<Link href='/technology'>
 						<li
-							onClick={() => setActive("technology")}
 							className={
-								active === "technology"
+								active === "/technology"
 									? "xl:flex xl:gap-3 xl:tracking-[2.7px] text-white tracking-[2px] cursor-pointer ease duration-100 border-b-white border-b-4 h-full py-[34px]"
 									: "xl:flex xl:gap-3 xl:tracking-[2.7px] tracking-[2px] text-white cursor-pointer ease duration-100 hover:border-b-white/50 border-b-transparent h-full py-[34px] border-b-4"
 							}>
@@ -78,8 +100,8 @@ const Navbar = () => {
 				<div
 					className={
 						nav
-							? "transition-all duration-100 ease-linear fixed bg-white/10 backdrop-blur-xl font-barlowCondensed z-[1000] top-0 bottom-0 right-0 left-[35%]  h-full"
-							: "transition-all duration-100 ease-linear fixed bg-white/10 backdrop-blur-xl font-barlowCondensed top-0 bottom-0 left-[100%]"
+							? "transition-all duration-300 ease-linear fixed bg-white/10 backdrop-blur-xl font-barlowCondensed z-[1000] top-0 bottom-0 right-0 left-[35%]  h-full"
+							: "transition-all duration-300 ease-linear fixed bg-white/10 backdrop-blur-xl font-barlowCondensed top-0 bottom-0 right-0 left-[100%]"
 					}>
 					<div className='px-6 py-8 flex items-center justify-end'>
 						<Image
@@ -92,42 +114,51 @@ const Navbar = () => {
 					</div>
 
 					<ul className='pl-4 uppercase w-full'>
-						<li
-							onClick={() => setActive("home")}
-							className={
-								active === "home"
-									? "flex gap-3 my-7 text-white tracking-[2.7px] cursor-pointer ease duration-100 border-r-white border-r-4 py-1"
-									: "flex gap-3 my-7 tracking-[2.7px] text-blue cursor-pointer ease duration-100 hover:border-r-white/50 border-r-transparent h-full py-1 border-r-4"
-							}>
-							<span className='font-bold '>00</span>home
-						</li>
-						<li
-							onClick={() => setActive("destination")}
-							className={
-								active === "destination"
-									? "flex gap-3 my-7 text-white tracking-[2.7px] cursor-pointer ease duration-100 border-r-white border-r-4 py-1"
-									: "flex gap-3 my-7 tracking-[2.7px] text-blue cursor-pointer ease duration-100 hover:border-r-white/50 border-r-transparent h-full py-1 border-r-4"
-							}>
-							<span className='font-bold'>01</span>destination
-						</li>
-						<li
-							onClick={() => setActive("crew")}
-							className={
-								active === "crew"
-									? "flex gap-3 my-7 text-white tracking-[2.7px] cursor-pointer ease duration-100 border-r-white border-r-4 py-1"
-									: "flex gap-3 my-7 tracking-[2.7px] text-blue cursor-pointer ease duration-100 hover:border-r-white/50 border-r-transparent h-full py-1 border-r-4"
-							}>
-							<span className='font-bold'>02</span>crew
-						</li>
-						<li
-							onClick={() => setActive("technology")}
-							className={
-								active === "technology"
-									? "flex gap-3 my-7 text-white tracking-[2.7px] cursor-pointer ease duration-100 border-r-white border-r-4 py-1"
-									: "flex gap-3 my-7 tracking-[2.7px] text-blue cursor-pointer ease duration-100 hover:border-r-white/50 border-r-transparent h-full py-1 border-r-4"
-							}>
-							<span className='font-bold'>03</span>technology
-						</li>
+						<Link href='/'>
+							<li
+								onClick={() => setNav(false)}
+								className={
+									active === "/"
+										? "flex gap-3 my-7 text-white tracking-[2.7px] cursor-pointer ease duration-100 border-r-white border-r-4 py-1"
+										: "flex gap-3 my-7 tracking-[2.7px] text-blue cursor-pointer ease duration-100 hover:border-r-white/50 border-r-transparent h-full py-1 border-r-4"
+								}>
+								<span className='font-bold '>00</span>home
+							</li>
+						</Link>
+						<Link href='/destination'>
+							<li
+								onClick={() => setNav(false)}
+								className={
+									active === "/destination"
+										? "flex gap-3 my-7 text-white tracking-[2.7px] cursor-pointer ease duration-100 border-r-white border-r-4 py-1"
+										: "flex gap-3 my-7 tracking-[2.7px] text-blue cursor-pointer ease duration-100 hover:border-r-white/50 border-r-transparent h-full py-1 border-r-4"
+								}>
+								<span className='font-bold'>01</span>destination
+							</li>
+						</Link>
+
+						<Link href='/crew'>
+							<li
+								onClick={() => setNav(false)}
+								className={
+									active === "/crew"
+										? "flex gap-3 my-7 text-white tracking-[2.7px] cursor-pointer ease duration-100 border-r-white border-r-4 py-1"
+										: "flex gap-3 my-7 tracking-[2.7px] text-blue cursor-pointer ease duration-100 hover:border-r-white/50 border-r-transparent h-full py-1 border-r-4"
+								}>
+								<span className='font-bold'>02</span>crew
+							</li>
+						</Link>
+						<Link href='/technology'>
+							<li
+								onClick={() => setNav(false)}
+								className={
+									active === "/technology"
+										? "flex gap-3 my-7 text-white tracking-[2.7px] cursor-pointer ease duration-100 border-r-white border-r-4 py-1"
+										: "flex gap-3 my-7 tracking-[2.7px] text-blue cursor-pointer ease duration-100 hover:border-r-white/50 border-r-transparent h-full py-1 border-r-4"
+								}>
+								<span className='font-bold'>03</span>technology
+							</li>
+						</Link>
 					</ul>
 				</div>
 			</div>
