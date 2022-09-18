fe0e61ef89fb583c2840b95c80da62de485dc491
Squashim <konrad18v1@wp.pl>
2022-09-18 15:50:53 +0200

    Finished

--- components/ExploreBtn.js
+++ components/ExploreBtn.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const ExploreBtn = () => {
 	return (
-		<Link href='/destination'>
+		<Link href='/Destination'>
 			<button className='transition-all duration-200 ease-linear xs:mt-[9.75rem] mt-[5rem] flex items-center justify-center xs:w-[15.125rem] w-[9.375rem]  xl:w-[20rem] aspect-square bg-white rounded-full cursor-pointer hover:shadow-exploreBtnShadow '>
 				<h4 className='font-bellefair text-[1.25rem] tracking-[1.25px] lg:tracking-[2px] xs:text-[2rem] uppercase text-dark '>
 					explore
